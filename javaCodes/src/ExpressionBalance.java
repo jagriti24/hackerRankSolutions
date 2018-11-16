b584527ac33e78f5d977e0c579e51b390bdd81d0
jagriti.singh <jagriti.singh@flipkart.com>
2018-11-16 22:04:57 +0530

    first code expression balancing

---
+++ javaCodes/src/ExpressionBalance.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+public class ExpressionBalance {
+
+
+    public static Integer balance(String input){
+        System.out.println(input);
+
+        if(input.length()%2 != 0){
+            return -1;
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+       for(int i = 0; i< input.length(); i++){
+
+           if( input.charAt(i) == '}'  && !stack.empty()){
+               if(stack.peek() == '{')
+                   stack.pop();
+           }
+           else {
+               stack.push(input.charAt(i));
+           }
+       }
+       int originalStackLength = stack.size();
+
+       int x = 0;
+       while(!stack.isEmpty() && stack.peek()=='{'){
+           stack.pop(); x++;
+       }
+
+      return  (originalStackLength/2) + (x%2);
+    }
+
+
+    public static void main(String[] args) {
+
+        String input = new String("}{{{}}}{{{");
+        Integer reversals =  balance(input);
+
+        System.out.println("No of Reversals :"+ reversals);
+    }
+}
